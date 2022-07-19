4ee3bd3379dd6e8ed72c952d9bb42daa8fd77de7
Arseniy <romanyuk@progressivemedia.ru>
2022-07-19 20:31:35 +0300

    Js modules added

---
+++ database/migrations/2022_06_14_232343_create_project_file.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_file', static function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('project_id')->unsigned();
+            $table->bigInteger('file_id')->unsigned()->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_file');
+    }
+};
